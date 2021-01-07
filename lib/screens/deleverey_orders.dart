6dbc7195cc760382f15f43197a7f50ca88d769f5
panchakarlavishnendra <vishnendra.p@walkingtree.tech>
2021-01-07 15:44:50 +0530

    added search in delevery order screen.

--- lib/screens/deleverey_orders.dart
+++ lib/screens/deleverey_orders.dart
@@ -28,110 +28,150 @@ class _SelectWarehouseContacts extends State<PendingApprovals> {
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(title: Text('Delevery Options')),
-      body:SingleChildScrollView(
-    child: Card(
-      child: Container(
-      color: Colors.black,
-        height: 250.0,
-        width: double.infinity,
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
+      body:ListView(
+        children: <Widget>[
+          Container(
 
-                  Container(
-                    color: Colors.green,
-                    height: 50.0,
-                    width: double.infinity,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top:18.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text('Kamesh Cotton Ltd'),
-
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      color:Colors.white,
-                      child: Table(
-
-                        children: [
-
-                          TableRow(
-
-                              children: [
-                            Column(),
-                            Column(),
-                            Column(children:[
-                    //           RaisedButton(
-                    //         textColor: Colors.white,
-                    //         color: Colors.green,
-                    //
-                    //         child: Text('Enabled Button' ),
-                    // ),
-                              FlatButton(
-                                child: Text('Flat Button', style: TextStyle(fontSize: 20.0),),
-                                color: Colors.blueAccent,
-                                textColor: Colors.white,
-                                onPressed: () {},
-                              ),
-                ]),
-                          ]),
-                          TableRow( children: [
-                            Column(
-
-                                children:[Text('Contract')]),
-                            Column(children:[Text(':')]),
-                            Column(children:[Text('john doe')]),
-                          ]),
-                          TableRow( children: [
-                            Column(children:[Text('Commodity')]),
-                            Column(children:[Text(':')]),
-                            Column(children:[Text("paddy")]),
-                          ]),
-                          TableRow( children: [
-                            Column(children:[Text('Date')]),
-                            Column(children:[Text(':')]),
-                            Column(children:[Text('20-01-2020')]),
-                          ]),TableRow( children: [
-                            Column(children:[Text('Toatal Trucks')]),
-                            Column(children:[Text(':')]),
-                            Column(children:[Text('5')]),
-                          ]),
-                          TableRow( children: [
-                            Column(children:[Text('Total Weights')]),
-                            Column(children:[Text(':')]),
-                            Column(children:[Text('5680MT')]),
-                          ]),TableRow( children: [
-                            Column(),
-                            Column(),
-                            Padding(
-                              padding: const EdgeInsets.only(top:18.0),
-                              child: Column(children:[Icon(
-                                Icons.car_rental,
-                                size: 30.0,
-                              ),]),
-                            ),
-                          ]),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+            child: Padding(
+              padding: const EdgeInsets.only(left:28.0),
+              child: TextField(
+//             onChanged: (value) {
+//               setState(() {
+//                 filteredList.clear(); //for the next time that we search we want the list to be unfilterted
+//                 filteredList.addAll(autoList); //getting list to original state
+//
+// //removing items that do not contain the entered Text
+//                 filteredList.removeWhere((i) => i.contains(value.toString())==false);
+//
+// //following is just a bool parameter to keep track of lists
+//                 searched=!searched;
+//               });
+//
+//
+//             },
+//             controller: editingController,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  labelText: "Search",
+                  // prefixIcon: Icon(Icons.search),
+                ),
               ),
             ),
+          ),
+    Padding(
+      padding: const EdgeInsets.only(left:18.0),
+      child: Container(
 
-          ],
-        ),
-      ),
+          child:Card(
+              child: Container(
+              color: Colors.white,
+                height: 250.0,
+                width: double.infinity,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+
+                          Container(
+                            color: Colors.green,
+                            height: 50.0,
+                            width: double.infinity,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top:18.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.only(left:18.0),
+                                    child: Text('Kamesh Cotton Ltd'),
+                                  ),
+
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              color:Colors.white,
+                              child: Table(
+
+                                children: [
+
+                                  TableRow(
+
+                                      children: [
+                                    Column(),
+                                    Column(),
+                                    Column(children:[
+                            //           RaisedButton(
+                            //         textColor: Colors.white,
+                            //         color: Colors.green,
+                            //
+                            //         child: Text('Enabled Button' ),
+                            // ),
+                                      FlatButton(
+                                        child: Text('Flat Button', style: TextStyle(fontSize: 18.0),),
+                                        color: Colors.blueAccent,
+                                        textColor: Colors.white,
+                                        onPressed: () {},
+                                      ),
+                        ]),
+                                  ]),
+                                  TableRow( children: [
+                                    Column(
+
+                                        children:[Text('Contract')]),
+                                    Column(children:[Text(':')]),
+                                    Column(children:[Text('john doe')]),
+                                  ]),
+                                  TableRow( children: [
+                                    Column(children:[Text('Commodity')]),
+                                    Column(children:[Text(':')]),
+                                    Column(children:[Text("paddy")]),
+                                  ]),
+                                  TableRow( children: [
+                                    Column(children:[Text('Date')]),
+                                    Column(children:[Text(':')]),
+                                    Column(children:[Text('20-01-2020')]),
+                                  ]),TableRow( children: [
+                                    Column(children:[Text('Toatal Trucks')]),
+                                    Column(children:[Text(':')]),
+                                    Column(children:[Text('5')]),
+                                  ]),
+                                  TableRow( children: [
+                                    Column(children:[Text('Total Weights')]),
+                                    Column(children:[Text(':')]),
+                                    Column(children:[Text('5680MT')]),
+                                  ]),TableRow( children: [
+                                    Column(),
+                                    Column(),
+                                    Padding(
+                                      padding: const EdgeInsets.only(top:18.0),
+                                      child: Column(children:[Icon(
+                                        Icons.car_rental,
+                                        size: 30.0,
+                                      ),]),
+                                    ),
+                                  ]),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+            ),
     ),
+
+    ],
     ),
 
       endDrawer: Theme(
